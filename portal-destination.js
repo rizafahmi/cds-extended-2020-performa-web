b889ade0edebd3be631852cd0e265005612dc36a
Riza Fahmi <rizafahmi@gmail.com>
2021-01-11 20:45:20 +0700

    Finish portal example

--- portal-destination.js
+++ portal-destination.js
@@ -2,15 +2,60 @@ addEventListener('portalactivate', function(event) {
   const portal = event.adoptPredecessor();
   document.body.append(portal);
 
+  const div = document.createElement('div');
   const button = document.createElement('button');
-  button.textContent = "Share";
+  button.textContent = "Donasi";
+  const input = document.createElement('input');
+  input.type = 'number';
+  const rp = document.createElement('span');
+  rp.textContent = "Rp. ";
+  const header = document.createElement('h1')
+  header.textContent = "Donasi"
+
+  button.addEventListener('click', function() {
+    portal.activate();
+  });
+
+
+  div.append(header);
+  div.append(rp);
+  div.append(input);
+  div.append(button);
+
   const style = document.createElement('style');
   style.innerHTML = `
     button {
-        position: absolute;
-        left: 2em;
-        top: 2em;
+      background-color: #fff382;
+      border-radius: 4px;
+      border: 1px solid #2779bd;
+      color: #2779bd;
+      padding-top: 4px;
+      padding-bottom: 4px;
+      font-weight: bold;
+    }
+    input {
+      border-radius: 4px;
+      border: 1px solid #3d4852;
+      padding: 4px;
+      margin-right: 1em;
+    }
+    span {
+      font-weight: bold;
+    }
+    div {
+      padding: 1em;
+      background-color: #fff;
+      position: absolute;
+      left: 25em;
+      top: 12em;
+      width: 300px;
+      height: 128px;
+      border-radius: 1em;
+      box-shadow: 0 0 20px 10px #999;
+    }
+    body {
+      color: #3d4852;
     }
 `;
-  document.body.append(style, button);
+  document.body.append(style, div);
 })
--- portal.js
+++ portal.js
@@ -1,10 +1,8 @@
-(async function() {
 const portal = document.createElement('portal');
 portal.src = 'http://localhost:8080/blank.html';
 document.body.append(portal);
-  setTimeout(function() {
-portal.activate();
-    
-  }, 1000);
-  
-})();
+
+const shareButton = document.querySelector("#share-button");
+shareButton.addEventListener('click', function() {
+  portal.activate();
+});
